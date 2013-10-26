3069a5e2f58e21c4096980bcb86ecd600747438c
Albert Albala <albert@mediatribe.net>
2013-10-26 12:48:29 -0400

    removed user block

--- sites/all/modules/custom/demo_deploy/ci-scripts/check-master.sh
+++ sites/all/modules/custom/demo_deploy/ci-scripts/check-master.sh
@@ -1,6 +1,11 @@
 # see https://github.com/drush-ops/drush/issues/212 for the use of grep -v 
-
 # this won't work unless you set your base_url in sites/default/settings.php
+
+drush vset maintenance_mode 1
+drush updb -y
+drush cc all
+drush cron
+drush vset maintenance_mode 0
 
 MYTESTGROUP=demo
 RESULT=$(drush test-run $MYTESTGROUP 2>&1)
